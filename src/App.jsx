c9144ebe814ed4c868416eddb65396d38c9af322
unknown <b.wilki0109@gmail.com>
2025-09-05 11:56:21 +0200

    Reducing state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 import Log from "./components/Log"
 
+function deriveActivePlayer(turns) {
+  let activePlayer = 'X'
+
+  if(turns.length > 0 && turns[0].player === 'X') {
+    activePlayer = 'O'
+  }
+
+  return activePlayer
+}
+
 function App() {
   const [turns, setTurns] = useState([])
-  const [activePlayer, setActivePlayer] = useState('X')
+
+  const activePlayer = deriveActivePlayer(turns)
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
-    
     setTurns((prevTurns) => {
-      let activePlayer = 'X'
-
-      if(turns.length > 0 && turns[0].player === 'X') {
-        activePlayer = 'O'
-      }
+      const activePlayer = deriveActivePlayer(prevTurns)
 
       const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: activePlayer},...prevTurns]
 
